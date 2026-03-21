5d6463059c4e4eb7407f69716a8e0fdb0264ab5f
blaubeen <blau6610@gmail.com>
2026-03-21 16:20:39 +0900

    fix

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import {  useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { db } from "../../firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
